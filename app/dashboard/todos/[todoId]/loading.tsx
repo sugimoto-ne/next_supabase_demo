cd41b47f762d7db17657c57d311288e2af8ab951
sugimo <sugimo.324@gmail.com>
2023-04-04 18:03:12 +0900

    add loading

---
+++ app/dashboard/todos/[todoId]/loading.tsx
@@ -0,0 +1,10 @@
+import { Skelton } from "../../../components/Skelton";
+const loading = () => {
+  return (
+    <div className="p-10">
+       <Skelton />
+    </div>
+  );
+}
+
+export default loading;
